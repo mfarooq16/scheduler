4eb21c2be7bf67384380301d039adf56dcfad9b0
Maham Farooq <maham.16491@gmail.com>
2020-09-28 20:18:40 -0400

    formatSpots function added to DayListItem Component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -10,10 +10,22 @@ export default function DayListItem(props) {
     "day-list__item--full ": props.spots === 0
   });
 
+  const formatSpots = (spots) => {
+    let string = "";
+    if(spots === 0) {
+      return string = "no spots remaining"
+    } else if(spots === 1) {
+      return string = "1 spot remaining"
+    } else {
+      return string = spots + " spots remaining"
+    };
+  }
+
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
