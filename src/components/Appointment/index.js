4033f10a75667e540256b1be58104e8e28e96d22
Maham Farooq <maham.16491@gmail.com>
2020-09-29 23:09:42 -0400

    Appointment Empty and BookedStories Added

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,13 +2,18 @@ import React from "react";
 
 import "components/Appointment/styles.scss";
 
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
+
 const classNames = require("classnames");
 
 export default function Appointment(props) {
-
+  const fragment = props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />;
   return (
   <article className="appointment">
-    
+    <Header time={props.time} />
+    {fragment}
   </article>
-  )
+  );
 }
