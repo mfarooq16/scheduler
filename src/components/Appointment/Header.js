3814172e89335f8c8ee0fcabd14239d4eebb05ff
Maham Farooq <maham.16491@gmail.com>
2020-09-29 20:20:26 -0400

    Header component created for Appointment

---
+++ src/components/Appointment/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+//import "components/Appointment/styles.scss";
+
+//const classNames = require("classnames");
+
+export default function Header(props) {
+
+  return (
+    <header className="appointment__time">
+      <h4 className="text--semi-bold">{props.time}</h4>
+      <hr className="appointment__separator" />
+    </header>
+  )
+}
