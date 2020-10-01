235656172ee7cdb3795244948046da0c2c740a32
Maham Farooq <maham.16491@gmail.com>
2020-09-30 21:48:05 -0400

    combined state

--- src/components/Application.js
+++ src/components/Application.js
@@ -38,8 +38,17 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  //const [day, setDay] = useState("Monday");
+  //const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // you may put the line below, but will have to remove/comment hardcoded appointments variable
+    appointments: {}
+  });
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
+
 
   const appointmentsList = appointments.map((appointment) => { return <Appointment key={appointment.id} {...appointment} />});
 
@@ -59,8 +68,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             setDay={setDay}
           />
         </nav>
