6a902cb2b797da837af7a4bfceb573d55669450a
Maham Farooq <maham.16491@gmail.com>
2020-10-01 22:03:21 -0400

    used useVisualMode to implement SHOW and EMPTY mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,14 +6,29 @@ import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 
+import useVisualMode from "hooks/useVisualMode";
+
 const classNames = require("classnames");
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
 
 export default function Appointment(props) {
-  const fragment = props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />;
+  //const fragment = props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />;
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
-  <article className="appointment">
-    <Header time={props.time} />
-    {fragment}
-  </article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+    </article>
   );
 }
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,16 +4,6 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
   
-  /*const transition = (newMode, replace = false) => {
-    (!replace) ? history.push(newMode) : history.splice(-1, 1, newMode);
-    setMode(newMode);
-  };
-
-  const back = () => {
-    history > initial && history.pop();
-    setMode(history.slice(-1)[0]);
-  }*/
-
   function transition(newMode, replace = false) {
     if (replace) {
       history[history.length - 1] = newMode;
