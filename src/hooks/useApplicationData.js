4da6ff397a9b7c4e5c987fb61cdd56b6a7143073
Maham Farooq <maham.16491@gmail.com>
2020-10-06 21:43:39 -0400

    Mocking Modules - adding fake data to test useApplicationData Hook

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -68,15 +68,16 @@ export default function useApplicationData() {
 
   useEffect (() => {
     Promise.all([
-      axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments"),
-      axios.get("http://localhost:8001/api/interviewers")
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ]).then(
       (all) => {
         let [days, appointments, interviewers] = all;
         days = days.data;
         appointments = appointments.data;
         interviewers = interviewers.data;
+        
         setState((prev) => ({ ...prev, days, appointments, interviewers }));
       }
     );
